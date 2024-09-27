79f1125c47335c44281da1c8ac9c08149ab1f721
Paul Rysiavets <paul@covadem.com>
2024-09-27 15:40:34 +0200

    fix: update setup_view

--- custom_components/neerslag/const.py
+++ custom_components/neerslag/const.py
@@ -1,5 +1,5 @@
 """Constants for the Neerslag Sensor (Buienalarm / Buienradar) integration."""
 
 DOMAIN = "neerslag"
-FRONTEND_SCRIPT_URL = "/neerslag-card.js"
+FRONTEND_SCRIPT_URL = "/api/neerslag-card/neerslag-card.js"
 DATA_EXTRA_MODULE_URL = 'frontend_extra_module_url'
